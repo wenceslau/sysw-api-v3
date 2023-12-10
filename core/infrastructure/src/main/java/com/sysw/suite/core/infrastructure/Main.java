764ca48c7bbf37d80dca961ac9c735404904f9ac
Wenceslau Neto <wenceslau.neto@metyis.com>
2023-12-10 15:48:59 +0000

    Add server and database configurations to application
    This commit introduces configurations to the application for development, production and test environments. It includes server settings (such as port configurations and thread worker details), spring container optimizations, and database connection parameters. Furthermore, a `docker-compose.yaml` file for a mysql service, new SQL migration scripts, and updates to the `pom.xml` file for new dependencies and build settings have been added. The main application class is also updated to boot up with the new configuration.

--- core/infrastructure/src/main/java/com/sysw/suite/core/infrastructure/Main.java
+++ core/infrastructure/src/main/java/com/sysw/suite/core/infrastructure/Main.java
@@ -1,7 +1,15 @@
 package com.sysw.suite.core.infrastructure;
 
+import com.sysw.suite.core.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "test");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ core/infrastructure/src/main/java/com/sysw/suite/core/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.sysw.suite.core.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.sysw.suite.core")
+public class WebServerConfig {
+}

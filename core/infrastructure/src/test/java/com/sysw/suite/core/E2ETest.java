eafe9d78459fd50bdb9bcfba782f8471e4fe3af7
Wenceslau Neto <wenceslau.neto@metyis.com>
2023-12-25 21:10:34 +0000

    --Add application configuration and test classes for end-to-end tests

---
+++ core/infrastructure/src/test/java/com/sysw/suite/core/E2ETest.java
@@ -0,0 +1,20 @@
+package com.sysw.suite.core;
+import com.sysw.suite.core.infrastructure.configuration.WebServerConfig;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test-e2e")
+@SpringBootTest(classes = WebServerConfig.class)
+@ExtendWith(MySQLCleanUpExtension.class)
+@AutoConfigureMockMvc
+@Tag("e2eTest")
+public @interface E2ETest {
+}
---
+++ core/infrastructure/src/test/java/com/sysw/suite/core/e2e/category/CategoryE2ETest.java
@@ -0,0 +1,32 @@
+package com.sysw.suite.core.e2e.category;
+
+import com.sysw.suite.core.E2ETest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@E2ETest
+@Testcontainers
+public class CategoryE2ETest {
+
+    @Container
+    private static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer("mysql:latest")
+            .withPassword("123456")
+            .withUsername("root")
+            .withDatabaseName("adm_videos");
+
+    @DynamicPropertySource
+    public static void setDatasourceProperties(final DynamicPropertyRegistry registry) {
+        registry.add("mysql.port", () -> MYSQL_CONTAINER.getMappedPort(3306));
+    }
+
+    @Test
+    void testWorks() {
+        System.out.println("It works!");
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+    }
+}
